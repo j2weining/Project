280f3fdb26ab45b9af49d8c67a43fe12dd8452fa
j2weining <weining2001@gmail.com>
2021-06-19 01:55:39 +0800

    project

--- function.js
+++ function.js
@@ -1,72 +1,69 @@
 function calculate() {
-    let name = document.getElementById('name').value;
-    let tele = document.getElementById('tele').value;
-    let address = document.getElementById('address').value;
-    let pname1 = document.getElementById('pname1').value;
-    let price1 = document.getElementById('price1').value;
-    let quantity1 = document.getElementById('quantity1').value;
-    let pname2 = document.getElementById('pname2').value;
-    let price2 = document.getElementById('price2').value;
-    let quantity2 = document.getElementById('quantity2').value;
+    let selection1 = confirm('Are you sure to calculate?');
+    if (selection1 == true) {
 
-    var total1 = price1 * quantity1;
-    var total2 = price2 * quantity2;
-    var GTotal = total1 + total2;
+        let name = document.getElementById('name').value;
+        let tele = document.getElementById('tele').value;
+        let address = document.getElementById('address').value;
+        let pname1 = document.getElementById('pname1').value;
+        let price1 = document.getElementById('price1').value;
+        let quantity1 = document.getElementById('quantity1').value;
+        let pname2 = document.getElementById('pname2').value;
+        let price2 = document.getElementById('price2').value;
+        let quantity2 = document.getElementById('quantity2').value;
 
-    if (name == '') {
-        alert('Please enter your name.')
-    } else if (tele == '') {
-        alert('Please enter your telephone number.')
-    } else if (address == '') {
-        alert('Please enter your address.')
-    } else if (pname1 == '') {
-        alert('Please enter product name')
-    } else if (price1 == '') {
-        alert('Please enter product price.')
-    } else if (quantity1 == '') {
-        alert('Please enter product quantity.')
-    } else if (pname2 == '') {
-        alert('Please enter product name.')
-    } else if (price2 == '') {
-        alert('Please enter product price')
-    } else if (quantity2 == '') {
-        alert('Please enter product quantity.')
-    } else {
-        document.getElementById('oname').innerHTML = name;
-        document.getElementById("otele").innerHTML = tele;
-        document.getElementById('oaddress').innerHTML = address;
-        document.getElementById('opname1').innerHTML = pname1;
-        document.getElementById('oprice1').innerHTML = price1;
-        document.getElementById('oquantity1').innerHTML = quantity1;
-        document.getElementById('total1').innerHTML = 'RM' + total1;
-        document.getElementById('opname2').innerHTML = pname2;
-        document.getElementById('oprice2').innerHTML = price2;
-        document.getElementById('oquantity2').innerHTML = quantity2;
-        document.getElementById('total2').innerHTML = 'RM' + total2;
-        document.getElementById('GTotal').innerHTML = 'RM' + GTotal;
+        var total1 = price1 * quantity1;
+        var total2 = price2 * quantity2;
+        var GTotal = total1 + total2;
+
+        if (name != "") {
+            if (tele != "") {
+                if (address != "") {
+                    if (pname1 && pname2 != "") {
+                        if(price1 && price2 != ""){
+                            if(quantity1 && quantity2 != ""){
+                                document.getElementById('oname').innerHTML = name;
+                                document.getElementById("otele").innerHTML = tele;
+                                document.getElementById('oaddress').innerHTML = address;
+                                document.getElementById('opname1').innerHTML = pname1;
+                                document.getElementById('oprice1').innerHTML = price1;
+                                document.getElementById('oquantity1').innerHTML = quantity1;
+                                document.getElementById('total1').innerHTML = 'RM' + total1;
+                                document.getElementById('opname2').innerHTML = pname2;
+                                document.getElementById('oprice2').innerHTML = price2;
+                                document.getElementById('oquantity2').innerHTML = quantity2;
+                                document.getElementById('total2').innerHTML = 'RM' + total2;
+                                document.getElementById('GTotal').innerHTML = 'RM' + GTotal;
+                            }else{
+                                alert('Please enter product quantity.')
+                                location.reload()
+                            }
+                        }else{
+                            alert('Please enter product price')
+                            location.reload()
+                        }
+                    }else{
+                        alert('Please enter product name.')
+                        location.reload()
+                    }
+                }else{
+                    alert('Please enter your address.')
+                    location.reload()
+                }
+            }else{
+                alert('Please enter your telephone number.')
+                location.reload()
+            }
+        }else{
+            alert('Please enter your name.')
+            location.reload()
+        }
+    }
+    else{
+        alert('Sorry, Please try again.')
+        location.reload()
     }
 }
-
-function reset() {
+function reset(){
     location.reload()
-}
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
